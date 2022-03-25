d39d67c8d41cd57f839b2b692d87d4b0c9d9b491
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2022-03-25 21:03:15 +0300

    ModalOverlay вынесен в отдельный файл

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,35 +1,9 @@
-import React, {useCallback, useEffect} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import cl from './modal.module.css';
 import {CloseIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from "prop-types";
-
-/**
- * ModalOverlay — фоновая подложка под модальным окном.
- */
-const ModalOverlay = ({children, closeHandler = () => null}) => {
-	
-	// 1. Клик по дочернему элементу не должен вызывать `closeHandler`
-	const onClick = (event) => event.target === event.currentTarget && closeHandler();
-	
-	// 2. Закрытие по ESC
-	const onCloseByEsc = useCallback(event => event.keyCode === 27 && closeHandler(), []);
-	
-	useEffect(() => {
-		document.addEventListener('keydown', onCloseByEsc);
-		return () => document.removeEventListener('keydown', onCloseByEsc);
-	}, [])
-	
-	return (
-		<div className={cl.overlay} onClick={onClick}>
-			{children}
-		</div>
-	);
-};
-
-ModalOverlay.propTypes = {
-	closeHandler: PropTypes.func
-};
+import ModalOverlay from './modal-overlay';
 
 /**
  * Modal — компонент самого модального окна: шапка с заголовком и иконка закрытия.
