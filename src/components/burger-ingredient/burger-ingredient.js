0e3f6d1c160d74c0bf0bc0253aa3489daecfe30e
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2022-04-06 21:22:07 +0300

    feat: При успешном перетаскивании у ингредиента в BurgerConstructor увеличивается счётчик.

--- src/components/burger-ingredient/burger-ingredient.js
+++ src/components/burger-ingredient/burger-ingredient.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {useDrag} from "react-dnd";
 import cl from "./burger-ingredient.module.css";
 import {Counter} from "@ya.praktikum/react-developer-burger-ui-components";
 import Price from "../price/price";
+import {useSelector} from "react-redux";
 
 const BurgerIngredient = ({ingredient}) => {
+
 	const [, dragRef] = useDrag({
 		type: "ingredient",
 		item: ingredient
 	});
 
+	const orderIngredients = useSelector(state => state.order.ingredients);
+	const count = useMemo(() =>
+		orderIngredients.reduce((function (previousValue, currentValue) {
+			return previousValue + (currentValue._id === ingredient._id ? 1 : 0);
+		}), 0), [orderIngredients]);
+
 	return (
 		<div draggable ref={dragRef} className={cl.item + ' relative'}>
-			<img src={ingredient.image} alt={ingredient.name} />
-			<Counter count={1} size="default" />
-			<Price value={ingredient.price} />
+			<img src={ingredient.image} alt={ingredient.name}/>
+			{count > 0 && <Counter count={count} size="default"/>}
+			<Price value={ingredient.price}/>
 			<p className="m-1">{ingredient.name}</p>
 		</div>
 	)
