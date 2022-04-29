16a8e1baba12c0843b05be43fa195dea6db0e51b
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2022-04-29 03:00:25 +0300

    feat: обновление информации о пользователе

--- src/services/sagas/index.js
+++ src/services/sagas/index.js
@@ -1,6 +1,7 @@
 import {all, call, fork, put, takeEvery, takeLatest} from 'redux-saga/effects'
 import * as ActionTypes from "../actions/userActions";
 import {
+	USER_PROFILE_UPDATE, USER_PROFILE_UPDATE_FAILURE, USER_PROFILE_UPDATE_SUCCESS,
 	userSetInfo,
 	userSignInFailureAction,
 	userSignOutFailureAction,
@@ -17,6 +18,21 @@ function* watchUserAuthentication() {
 	yield takeEvery(ActionTypes.USER_SIGN_OUT, userSignOutSaga);
 
 	yield takeEvery(ActionTypes.USER_TRY_AUTH, userAuth);
+	yield takeEvery(ActionTypes.USER_PROFILE_UPDATE, userProfileUpdateSaga);
+}
+
+function* userProfileUpdateSaga({payload}) {
+	try {
+		const result = yield call(UserService.update, payload);
+
+		if (result.success) {
+			yield put({type: USER_PROFILE_UPDATE_SUCCESS, payload: result.user})
+		} else {
+			yield put({type: USER_PROFILE_UPDATE_FAILURE, payload: result.message});
+		}
+	} catch (error) {
+		yield put({type: USER_PROFILE_UPDATE_FAILURE, payload: error.toString()});
+	}
 }
 
 function* userSignOutSaga() {
@@ -24,12 +40,10 @@ function* userSignOutSaga() {
 		const result = yield call(AuthService.logout);
 
 		if (result.success) {
-			yield call(AuthService.removeTokens);
-			yield put(userSignOutSuccessAction());
+			yield all([call(AuthService.removeTokens), put(userSignOutSuccessAction())]);
 		} else {
 			yield put(userSignOutFailureAction(result.message));
 		}
-
 	} catch (error) {
 		yield put(userSignOutFailureAction(error.toString()));
 	}
