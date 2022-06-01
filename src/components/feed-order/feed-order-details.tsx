afb0ad70527dded19d9675ff5424b58ffe1aceb3
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2022-06-01 15:16:08 +0300

    fix: в заказах нужно объединять повтоярющиеся ингредиенты в одну строку, умножая их цену на кол-во

--- src/components/feed-order/feed-order-details.tsx
+++ src/components/feed-order/feed-order-details.tsx
@@ -7,19 +7,26 @@ import cl from './feed-order.module.css';
 
 const FeedOrderDetails: React.FC = () => {
 
-    const {id} = useParams<{ id: string }>();
+    const {id} = useParams<{ id: any }>();
 
     const location = useLocation<{ feed: any }>();
     const isModal = location.state && location.state?.feed != null;
 
     const orders = useSelector((state) => state.feed.orders);
-    const ingredients = useSelector(state => state.ingredients);
-
-    // @ts-ignore
     const order = useMemo(() => orders.find(order => order.number == id), [orders, id]);
+
+    const allIngredients = useSelector(state => state.ingredients);
+    const ingredients = useMemo(() => allIngredients.filter(ingredient => order?.ingredients.includes(ingredient._id)), [order, allIngredients]);
+
+    const count = useMemo(() => {
+        return order?.ingredients.reduce(function (prev: { [k: string]: number }, cur) {
+            prev[cur] = (prev[cur] || 0) + 1;
+            return prev;
+        }, {}) || {};
+    }, [order]);
+
     const totalPrice = useMemo(() =>
-        order?.ingredients.reduce((value, item) =>
-            value + (ingredients.find(value => value._id == item)?.price || 0), 0) || 0, [order, ingredients]);
+        ingredients.reduce((value, ingredient) => value + ingredient.price * (count[ingredient._id] || 1), 0) || 0, [count, ingredients]);
 
     return order ? (
         <div className={isModal ? '' : cl.container}>
@@ -30,14 +37,12 @@ const FeedOrderDetails: React.FC = () => {
             </p>
             <h3>Состав:</h3>
             <div className={`${cl.details_container} custom-scroll`}>
-                {order.ingredients.map(item => {
-                    const ingredient = ingredients.find(value => value._id == item);
-                    return ingredient ? (<div className={cl.details_row} key={item}>
-                        <FeedOrderItem ingredient={ingredient}/>
-                        <div className={cl.details_row_title}>{ingredient.name}</div>
-                        <Price value={ingredient.price}/>
-                    </div>) : null
-                })}
+                {ingredients.map(ingredient => <div className={cl.details_row} key={ingredient._id}>
+                    <FeedOrderItem ingredient={ingredient}/>
+                    <div className={cl.details_row_title}>{ingredient.name}</div>
+                    <span className={'text_type_digits-default'}>{count[ingredient._id] || 1} x </span>
+                    <Price value={ingredient.price}/>
+                </div>)}
             </div>
             <div className={`${cl.date_container} mt-10`}>
                 <span className={'text_type_main-default'}>{new Date(order.createdAt).toLocaleString()}</span>
